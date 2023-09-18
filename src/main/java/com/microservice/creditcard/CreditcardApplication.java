56eab2cd8fa53be1e35bc8975ade9dbcf9cb3479
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-18 16:11:34 -0500

    Se registra el MS CreditCard en el servidor Eureka

--- src/main/java/com/microservice/creditcard/CreditcardApplication.java
+++ src/main/java/com/microservice/creditcard/CreditcardApplication.java
@@ -2,11 +2,13 @@ package com.microservice.creditcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Esta es la clase principal del proyecto.
  * */
+@EnableDiscoveryClient
 @SpringBootApplication
 //@EnableFeignClients
 public class CreditcardApplication {
